0757790f8f35459bd4dc785b0b328cc632d5f68f
Susana Medel <susana.medel@genstudents.org>
2024-05-09 15:01:54 -0400

    Entrega

--- README.md
+++ README.md
@@ -8,17 +8,21 @@ Debe solicitar al usuario la temperatura en grados Celsius por prompt o por un i
 Debe convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
 Debe imprimir ambos resultados por consola o por el DOM.
 Debe ser capaz de identificar si los datos de entrada sean de tipo number, en caso contrario debe mandar un mensaje de error y volver a solicitar los datos.
+
 Prueba tu programa con las siguientes entradas para asegurarte que funcione correctamente:
-Entrada: 
+Entrada:
+
 45.
 Salida
 Grados Kelvin: 318.15
 Grados Fahrenheit: 113. 
+
 Entrada:
  14.
 Salida
 Grados Kelvin: 287.15
 Grados Fahrenheit: 57.2. 
+
 Pruébalo con los valores numéricos que se te ocurran.
 Al final de tu práctica, tienes que subir el ejercicio a tu repositorio de GitHub.
 Colócalo en un repositorio llamado “logica-programacion-2”
---
+++ tarea.js
@@ -0,0 +1,37 @@
+// Debe solicitar al usuario la temperatura en grados Celsius por prompt o por un input.
+// Debe ser capaz de identificar si los datos de entrada sean de tipo number, 
+// en caso contrario debe mandar un mensaje de error y volver a solicitar los datos.
+
+
+function convertirTemperatura() {
+    let temperaturaCelsius = prompt("Ingrese la temperatura en grados Celsius:");
+
+    // Verificar si la entrada es un número
+    if (isNaN(temperaturaCelsius)) {
+        alert("Error: La temperatura ingresada no es un número.");  //console.error aparece en rojito en consola
+        convertirTemperatura(); // Volver a solicitar los datos
+        return;
+    }
+
+    // Convertir temperatura a grados Fahrenheit y Kelvin
+    let temperaturaFahrenheit = (parseFloat(temperaturaCelsius) * 9 / 5) + 32;
+    let temperaturaKelvin = parseFloat(temperaturaCelsius) + 273.15;
+
+    // Imprimir resultados
+    console.log("Grados Fahrenheit:", temperaturaFahrenheit);
+    console.log("Grados Kelvin:", temperaturaKelvin);
+}
+
+convertirTemperatura();
+
+// Debe convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
+// Debe imprimir ambos resultados por consola o por el DOM.
+// function fahrenheit(temp){
+//     fah = 9/5 * temp + 32
+// console.log("Grados Fahrenheit: " + fah)
+// }
+
+// function kelvin(temp){
+//     k = temp + 273.15
+//     console.log("Grados Kelvin: " + k)
+// }
